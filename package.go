da38372befd199f332a2c0fad50cc3ff32d00742
rachok <rachok2012@gmail.com>
2020-05-23 17:18:09 +0200

    hellow world

--- package.go
+++ package.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/stianeikeland/go-rpio"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("opening gpio")
+	err := rpio.Open()
+	if err != nil {
+		panic(fmt.Sprint("Unable to open gpio", err.Error()))
+	}
+
+	defer rpio.Close()
+
+	pin := rpio.Pin(12)
+	pin.Output()
+
+	for x := 0; x < 20; x++ {
+		pin.Toggle()
+		time.Sleep(time.Second / 5)
+	}
 }
